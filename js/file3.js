8ac6cfe6b9ffd1498ca9112a0dd64f476c1263d4
nairx <nairxprv@gmail.com>
2025-06-21 12:14:47 +0530

    first commit

---
+++ js/file3.js
@@ -0,0 +1,17 @@
+//conditional statements
+// let a = 10;
+// let b = 10;
+// if (a > b) console.log(`${a} is greater than ${b}`);
+// else if (b > a) console.log(`${b} is greater than ${a}`);
+// else console.log("Both are equal");
+
+// a > b ? console.log("a is greater") : console.log("b is greater");
+
+// let a = 9;
+// let b = 10;
+// a > b && console.log("a is greater");
+
+// let x=10
+let x=20
+let result = x || 5
+console.log(result)
