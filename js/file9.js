f57f820db049cb8eab0058ee630252371d8d38dd
nairx <nairxprv@gmail.com>
2025-06-21 22:11:42 +0530

    Updated

--- js/file9.js
+++ js/file9.js
@@ -1,4 +1,4 @@
-//arrays
+//arrays and methods
 // const score = [2, 1, 7, 5, 3];
 // console.log(score)
 // console.log(score[0])
@@ -7,25 +7,4 @@
 // console.log(score.length)
 // const newScore = [...score,9]
 // console.log(newScore)
-// const cart = {
-//   1: 5,
-//   5: 3,
-// };
-// const newCart = ({...cart,2:1})
-// console.log(newCart)
-
-let cart = {};
-const products = [
-  { id: 1, name: "Product 1", price: 25 },
-  { id: 2, name: "Product 2", price: 50 },
-  { id: 3, name: "Product 2", price: 75 },
-];
-//const newProducts = [...products,{id:4,name:"Product 4",price:56}]
-function addToCart(id) {
-  cart = { ...cart, [id]: 1 };
-}
-addToCart(1)
-console.log(cart)
-addToCart(3)
-console.log(cart)
-
+//shopping cart app
