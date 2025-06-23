2b645b38784ec020942971637cf74f4297e32d1c
nairx <nairxprv@gmail.com>
2025-06-23 14:36:21 +0530

    Updated

--- js/file9.js
+++ js/file9.js
@@ -8,3 +8,41 @@
 // const newScore = [...score,9]
 // console.log(newScore)
 //shopping cart app
+// const score = [2, 1, 7, 5, 3];
+// score.forEach((value) => {
+//   console.log(value);
+// });
+// score.map((value) => {
+//   console.log(value);
+// });
+// const score = [2, 1, 7, 5, 3];
+// const newScore = score.map((value) => {
+//   return value > 2;
+// });
+// console.log(newScore);
+
+// const score = [2, 1, 7, 5, 3];
+// const newScore = score.filter((value) => {
+//   return value > 2;
+// });
+// console.log(newScore);
+
+// const score = [2, 1, 7, 5, 2];
+// const newScore = score.find((value) => {
+//   return value === 2;
+// });
+// console.log(newScore);
+
+// const score = [2, 1, 7, 5, 2];
+// const newScore = score.reduce((sum,value) => {
+//   return sum + value
+// },0);
+// console.log(newScore);
+
+// const score = [2, 1, 7, 5, 2];
+// const newScore = score.reduce((sum, value) => sum + value, 0);
+// console.log(newScore);
+
+const score = [2, 1, 7, 5, 2];
+const newScore = score.find((value) => value === 2);
+console.log(newScore);
