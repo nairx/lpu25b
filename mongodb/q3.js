78a0f03b1730b1e6ec0edcf2c91eaaf41e029123
nairx <nairxprv@gmail.com>
2025-06-25 15:35:57 +0530

    Updated

--- mongodb/q3.js
+++ mongodb/q3.js
@@ -1,29 +1,33 @@
-db.employees.find()
-db.employees.find().skip(1)
-db.employees.find().skip(1).limit(1)
-db.employees.find({department:"IT"})
+db.employees.find();
+db.employees.find().skip(1);
+db.employees.find().skip(1).limit(1);
+db.employees.find({ department: "IT" });
 
 db.employees.find(
-    {department:{$eq:"IT"}},
-    {_id:0,name:1,salary:1}
-)
+  { department: { $eq: "IT" } },
+  { _id: 0, name: 1, salary: 1 }
+);
+
+db.employees.find({ salary: { $gt: 3000 } }, { _id: 0, name: 1, salary: 1 });
+
+db.employees.find({ salary: { $gte: 3000 } }, { _id: 0, name: 1, salary: 1 });
+
+db.employees.find({ salary: { $lt: 3000 } }, { _id: 0, name: 1, salary: 1 });
 
 db.employees.find(
-    {salary:{$gt:3000}},
-    {_id:0,name:1,salary:1}
-)
+  { salary: { $lte: 3000 }, department: "IT" },
+  { _id: 0, name: 1, salary: 1 }
+);
 
 db.employees.find(
-    {salary:{$gte:3000}},
-    {_id:0,name:1,salary:1}
-)
+  { $or: [{ salary: { $lte: 3000 } }, { department: "IT" }] },
+  { _id: 0, name: 1, salary: 1 }
+);
 
 db.employees.find(
-    {salary:{$lt:3000}},
-    {_id:0,name:1,salary:1}
+    {$or:[{},{}]}
 )
 
-db.employees.find(
-    {salary:{$lte:3000}},
-    {_id:0,name:1,salary:1}
-)
+db.employees.find({department:{$eq:"IT"}})
+
+db.employees.find({department:{$ne:"IT"}})
---
+++ mongodb/q4.js
@@ -0,0 +1,18 @@
+db.employees.insertMany([
+  {
+    name: "Amy",
+    email: "amy@gmail.com",
+    department: "HR",
+    salary: 2000,
+    location: ["NY", "TX"],
+    date: Date(),
+  },
+  {
+    name: "Rafeal",
+    email: "rafeal@gmail.com",
+    department: "Admin",
+    salary: 1500,
+    location: ["OH", "TX"],
+    date: Date(),
+  },
+]);
