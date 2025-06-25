eeb5eb0f69aeb06f3b6a8469e09c7f45af1bbf99
nairx <nairxprv@gmail.com>
2025-06-25 15:12:11 +0530

    updated

--- mongodb/q2.js
+++ mongodb/q2.js
@@ -7,6 +7,25 @@ db.employees.insertOne({
   date: Date(),
 });
 
-db.employees.find()
+db.employees.insertMany([
+  {
+    name: "Mike Joseph",
+    email: "mike@gmail.com",
+    department: "IT",
+    salary: 2456,
+    location: ["FL", "TX"],
+    date: Date(),
+  },
+  {
+    name: "Cathy G",
+    email: "cathy@gmail.com",
+    department: "IT",
+    salary: 3456,
+    location: ["AZ", "TX"],
+    date: Date(),
+  },
+]);
+
+db.employees.find();
 
 //show collections
---
+++ mongodb/q3.js
@@ -0,0 +1,29 @@
+db.employees.find()
+db.employees.find().skip(1)
+db.employees.find().skip(1).limit(1)
+db.employees.find({department:"IT"})
+
+db.employees.find(
+    {department:{$eq:"IT"}},
+    {_id:0,name:1,salary:1}
+)
+
+db.employees.find(
+    {salary:{$gt:3000}},
+    {_id:0,name:1,salary:1}
+)
+
+db.employees.find(
+    {salary:{$gte:3000}},
+    {_id:0,name:1,salary:1}
+)
+
+db.employees.find(
+    {salary:{$lt:3000}},
+    {_id:0,name:1,salary:1}
+)
+
+db.employees.find(
+    {salary:{$lte:3000}},
+    {_id:0,name:1,salary:1}
+)
