945204a06be7fe288445a3b35a255be7775db8d9
nairx <nairxprv@gmail.com>
2025-06-27 16:53:40 +0530

    updated

--- mongodb/q7.js
+++ mongodb/q7.js
@@ -7,4 +7,64 @@ db.posts.aggregate([
       as: "comments",
     },
   },
+  { $unwind: "$comments" },
+  { $project: { _id: 0, post: 1, "comments.comment": 1 } },
+]);
+
+db.marks.aggregate([
+  { $group: { _id: "$name", AvgScore: { $avg: "$score" } } },
+]);
+
+db.marks.aggregate([
+  { $group: { _id: "$term", AvgScore: { $avg: "$score" } } },
+  { $sort: { _id: 1 } },
+]);
+
+db.marks.aggregate([
+  { $group: { _id: "$subject", AvgScore: { $avg: "$score" } } },
+  { $sort: { _id: 1 } },
+]);
+
+db.marks.aggregate([
+  {
+    $group: {
+      _id: { term: "$term", subject: "$subject" },
+      AvgScore: { $avg: "$score" },
+    },
+  },
+  { $sort: { _id: 1 } },
+]);
+
+db.marks.aggregate([
+  { $match: { name: "John" } },
+  {
+    $group: {
+      _id: { term: "$term", name: "$name" },
+      AvgScore: { $avg: "$score" },
+    },
+  },
+  { $sort: { _id: 1 } },
+]);
+
+db.studentinfo.insertOne({ _id: "s1", name: "John" });
+db.studentinfo.insertOne({ _id: "s2", name: "Cathy" });
+
+db.marks.updateMany({}, { $rename: { name: "sid" } });
+
+db.marks.updateMany({ sid: "John" }, { $set: { sid: "s1" } });
+db.marks.updateMany({ sid: "Cathy" }, { $set: { sid: "s2" } });
+
+db.studentinfo.aggregate([
+  {
+    $lookup: {
+      from: "marks",
+      localField: "_id",
+      foreignField: "sid",
+      as: "marks",
+    },
+  },
+  {$unwind:"$marks"},
+  
+  {$group:{_id:"$marks.term",
+    AvgScore:{$avg:"$marks.score"}}}
 ]);
