ab6814ad2bb9adf0565fd5b943e6d147e79b38b8
nairx <nairxprv@gmail.com>
2025-07-01 16:57:41 +0530

    updated

--- mongodb/q13.js
+++ mongodb/q13.js
@@ -1,10 +1,27 @@
 use admin
+
 db.createUser({
   user: "admin",
   pwd: "admin",
   roles: [ { role: "root", db: "admin" } ] 
 })
+
+db.getUsers()
+
+
+
 add following settings in mongod.conf available in program files / mongodb
 security:
   authorization: enabled
 go to services and restart mongodb server
+
+
+use lpu
+
+db.createUser({
+  user: "user2",
+  pwd: "1234",
+  roles: [
+    { role: "read", db: "lpu" }
+  ]
+})
