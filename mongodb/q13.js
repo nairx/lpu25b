e3d90be42df5273924aff0453de69d5fd6495839
nairx <nairxprv@gmail.com>
2025-07-01 21:23:59 +0530

    updated

--- mongodb/q13.js
+++ mongodb/q13.js
@@ -1,27 +1,27 @@
-use admin
+// use admin
 
-db.createUser({
-  user: "admin",
-  pwd: "admin",
-  roles: [ { role: "root", db: "admin" } ] 
-})
+// db.createUser({
+//   user: "admin",
+//   pwd: "admin",
+//   roles: [ { role: "root", db: "admin" } ] 
+// })
 
-db.getUsers()
-
-
-
-add following settings in mongod.conf available in program files / mongodb
-security:
-  authorization: enabled
-go to services and restart mongodb server
+// db.getUsers()
 
 
-use lpu
 
-db.createUser({
-  user: "user2",
-  pwd: "1234",
-  roles: [
-    { role: "read", db: "lpu" }
-  ]
-})
+// add following settings in mongod.conf available in program files / mongodb
+// security:
+//   authorization: enabled
+// go to services and restart mongodb server
+
+
+// use lpu
+
+// db.createUser({
+//   user: "user2",
+//   pwd: "1234",
+//   roles: [
+//     { role: "read", db: "lpu" }
+//   ]
+// })
