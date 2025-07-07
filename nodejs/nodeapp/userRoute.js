1fce7c2906569790700b5e947ac8808888562443
nairx <nairxprv@gmail.com>
2025-07-07 15:44:23 +0530

    updated

--- nodejs/nodeapp/userRoute.js
+++ nodejs/nodeapp/userRoute.js
@@ -1,12 +1,127 @@
-import express from 'express'
-const Router = express.Router()
-Router.get("/show", (req, res) => {
-  res.send("Show Users API");
+import mongoose from "mongoose";
+import express from "express"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const SECRET = "something";
+
+const userRouter = express.Router();
+
+
+const userSchema = mongoose.Schema(
+  {
+    username: { type: String },
+    email: { type: String, unique: true },
+    password: { type: String },
+    role: { type: String, default: "user" },
+  },
+  { timestamps: true }
+);
+const userModel = mongoose.model("User", userSchema);
+
+const authenticate = (req, res, next) => {
+  try {
+    let token = req.headers.authorization;
+    token = token.split(" ")[1];
+    const user = jwt.verify(token, SECRET);
+    req.role = user.role;
+    next();
+  } catch (err) {
+    return res.json({ message: "Access Denied" });
+  }
+};
+
+const authorize = (role) => {
+  return (req, res, next) => {
+    if (req.role === role) {
+      next();
+    } else {
+      return res.json({ message: "Unauthorized Access" });
+    }
+  };
+};
+
+userRouter.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const hashedpwd = await bcrypt.hash(password, 10);
+    const user = {
+      username,
+      email,
+      password: hashedpwd,
+    };
+    const result = await userModel.create(user);
+    res.status(201).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 });
-Router.post("/register", (req, res) => {
-  res.send("Register User API");
+
+userRouter.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      const isMatch = await bcrypt.compare(password, existingUser.password);
+      if (isMatch) {
+        const userObj = {
+          username: existingUser.username,
+          email: existingUser.email,
+          role: existingUser.role,
+        };
+        const token = jwt.sign(userObj, SECRET, { expiresIn: "1h" });
+        res.status(200).json({ user: userObj, token });
+      } else {
+        res.status(400).json({ message: "Invalid Password" });
+      }
+    } else {
+      res.status(400).json({ message: "User not found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 });
-Router.post("/login", (req, res) => {
-  res.send("User Login API");
+
+userRouter.get("/showusers", authenticate, authorize("admin"), async (req, res) => {
+  try {
+    const result = await userModel.find();
+    res.status(200).json(result);
+  } catch (err) {}
 });
-export default Router
+
+userRouter.patch("/:id", authenticate, authorize("admin"), async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+    const result = await userModel.findByIdAndUpdate(id, body);
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+});
+
+userRouter.delete("/:id", authenticate, authorize("admin"), async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await userModel.findByIdAndDelete(id);
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+});
+
+userRouter.get("/:id/profile", authenticate, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await userModel.findOne({ _id: id });
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+});
+
+export default userRouter
