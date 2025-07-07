d8561d24a6c00c4d9ac696555ae227888df0ce7a
nairx <nairxprv@gmail.com>
2025-07-07 16:08:10 +0530

    updated

--- nodejs/nodeapp/userRoute.js
+++ nodejs/nodeapp/userRoute.js
@@ -1,80 +1,18 @@
-import mongoose from "mongoose";
 import express from "express";
-import bcrypt from "bcrypt";
 import { authenticate, authorize } from "./auth.js";
-import jwt from "jsonwebtoken";
-const SECRET = "something";
 const Router = express.Router();
-import userModel from "./userModel.js";
-import { register } from "./userController.js";
-
+import {
+  register,
+  login,
+  profile,
+  updateUser,
+  deleteUser,
+  showUsers,
+} from "./userController.js";
 Router.post("/register", register);
-
-Router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const existingUser = await userModel.findOne({ email });
-    if (existingUser) {
-      const isMatch = await bcrypt.compare(password, existingUser.password);
-      if (isMatch) {
-        const userObj = {
-          username: existingUser.username,
-          email: existingUser.email,
-          role: existingUser.role,
-        };
-        const token = jwt.sign(userObj, SECRET, { expiresIn: "1h" });
-        res.status(200).json({ user: userObj, token });
-      } else {
-        res.status(400).json({ message: "Invalid Password" });
-      }
-    } else {
-      res.status(400).json({ message: "User not found" });
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: "Something went wrong" });
-  }
-});
-
-Router.get("/showusers", authenticate, authorize("admin"), async (req, res) => {
-  try {
-    const result = await userModel.find();
-    res.status(200).json(result);
-  } catch (err) {}
-});
-
-Router.patch("/:id", authenticate, authorize("admin"), async (req, res) => {
-  try {
-    const id = req.params.id;
-    const body = req.body;
-    const result = await userModel.findByIdAndUpdate(id, body);
-    res.status(200).json(result);
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({ message: "Something went wrong" });
-  }
-});
-
-Router.delete("/:id", authenticate, authorize("admin"), async (req, res) => {
-  try {
-    const id = req.params.id;
-    const result = await userModel.findByIdAndDelete(id);
-    res.status(200).json(result);
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({ message: "Something went wrong" });
-  }
-});
-
-Router.get("/:id/profile", authenticate, async (req, res) => {
-  try {
-    const id = req.params.id;
-    const result = await userModel.findOne({ _id: id });
-    res.status(200).json(result);
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({ message: "Something went wrong" });
-  }
-});
-
+Router.post("/login", login);
+Router.get("/showusers", authenticate, authorize("admin"), showUsers);
+Router.patch("/:id", authenticate, authorize("admin"), updateUser);
+Router.delete("/:id", authenticate, authorize("admin"), deleteUser);
+Router.get("/:id/profile", authenticate, profile);
 export default Router;
